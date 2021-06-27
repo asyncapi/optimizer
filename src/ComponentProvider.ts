a59e1e23f139904b6003c5388464f484c6ad5e93
khudadad414 <khudadad414@gmail.com>
2021-06-27 18:41:27 +0530

    chore: ComponentProvider.ts implemented.

---
+++ src/ComponentProvider.ts
@@ -0,0 +1,89 @@
+import { Schema, Message, AsyncAPIDocument, ChannelParameter } from '@asyncapi/parser';
+
+/**
+ * This Singleton class will provide all sorts of data for optimizers.
+ *
+ * @public
+ */
+export class ComponentProvider {
+  messages = new Map<string, Message>();
+  schemas = new Map<string, Schema>();
+  parameters = new Map<string, ChannelParameter>();
+
+  private static instance: ComponentProvider;
+  private constructor(private document: AsyncAPIDocument) {
+    this.scanChannels();
+    this.scanComponents();
+  }
+  static getInstance(document: AsyncAPIDocument): ComponentProvider {
+    if (!ComponentProvider.instance) {
+      ComponentProvider.instance = new ComponentProvider(document);
+    }
+    return ComponentProvider.instance;
+  }
+
+  private scanChannels = (): void => {
+    const channels = this.document.channels();
+    for (const channelName in channels) {
+      const path = `#/channels/${channelName}`;
+      const channel = this.document.channel(channelName);
+      if (channel.hasPublish()) {
+        const message = channel.publish().message();
+        const currentPath = `${path}/publish/message`;
+        this.scanMessage(currentPath, message);
+        this.messages.set(currentPath,message);
+      }
+      if (channel.hasSubscribe()) {
+        const message = channel.subscribe().message();
+        const currentPath = `${path}/subscribe/message`;
+        this.scanMessage(currentPath, message);
+        this.messages.set(currentPath,message);
+      }
+      if (channel.hasParameters()) {
+        const currentPath = `${path}/parameters`;
+        for (const [name,channelParameter] of Object.entries(channel.parameters())) {
+          this.parameters.set(`${currentPath}/${name}`, channelParameter);
+        }
+      }
+    }
+  }
+  private scanMessage = (path: string, message: Message): void => {
+    const payload = message.payload();
+    const headers = message.headers();
+
+    if (payload) {
+      this.schemas.set(`${path}/payload`,payload);
+      const payloadProperties = payload.properties();
+      for (const [propertyName, propertySchema] of Object.entries(payloadProperties)) {
+        this.schemas.set(`${path}/payload/properties/${propertyName}`, propertySchema);
+      }
+    }
+    if (headers) {
+      this.schemas.set(`${path}/headers`,headers);
+      const headersProperties = headers.properties();
+      for (const [propertyName, propertySchema] of Object.entries(headersProperties)) {
+        this.schemas.set(`${path}/headers/properties/${propertyName}`, propertySchema);
+      }
+    }
+  }
+  private scanComponents = (): void => {
+    const components = this.document.components();
+    if (components) {
+      if (components.hasMessages()) {
+        for (const [messageName, message] of Object.entries(components.messages())) {
+          this.messages.set(`#/components/messages/${messageName}`, message);
+        }
+      }
+      if (components.hasSchemas()) {
+        for (const [schemaName, schema] of Object.entries(components.schemas())) {
+          this.schemas.set(`#/components/schemas/${schemaName}`, schema);
+        }
+      }
+      if (components.hasParameters()) {
+        for (const [parameterName, parameter] of Object.entries(components.parameters())) {
+          this.parameters.set(`#/components/parameters/${parameterName}`, parameter);
+        }
+      }
+    }
+  }
+}
