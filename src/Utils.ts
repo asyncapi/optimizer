ee939bbedf327fee4920bfd993d0549d26009b14
khudadad414 <khudadad414@gmail.com>
2021-07-08 19:06:00 +0530

    chore: Utils.ts refactored.

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,38 +1,40 @@
+
 /**
  * Compares two components.
  * @remarks
  *
  * this recursive function is responsible for comparing two component.
  *
- * @param component1 - the first component that we are going to compare.
- * @param component2 - the second component that we are going to compare.
+ * @param x - the first component that we are going to compare.
+ * @param y - the second component that we are going to compare.
  * @returns true, if both components are identical; false, if the components are not identical.
  *
  */
-const compareComponents = (component1: any, component2: any): boolean => {
-  try {
-    const keys1 = Object.keys(component1);
-    const keys2 = Object.keys(component2);
+const compareComponents = (x: any, y: any): boolean => {
+  if (!(x instanceof Object) || !(y instanceof Object)) {return false;}
+  // if they are not strictly equal, they both need to be Objects
+  for (const p in x) {
+    if (!Object.prototype.hasOwnProperty.call(x, p)) {continue;}
 
-    if (keys1.length !== keys2.length) {
-      return false;
-    }
+    if (!Object.prototype.hasOwnProperty.call(y, p)) {return false;}
+    // allows to compare x[ p ] and y[ p ] when set to undefined
 
-    for (const key of keys1) {
-      if (!key.startsWith('x-')) {
-        const objectType = typeof component1[String(key)] === 'object';
-        if (objectType) {
-          if (!compareComponents(component1[String(key)], component2[String(key)])) {
-            return true;
-          }
-        } else if (component1[String(key)] !== component2[String(key)]) {
-          return false;
-        }
-      }
-    }
-  } catch (err) {
-    return false;
+    if (x[String(p)] === y[String(p)] || p.startsWith('x-')) {continue;}
+    // if they have the same strict value or identity then they are equal
+
+    if (typeof (x[String(p)]) !== 'object') {return false;}
+    // Numbers, Strings, Functions, Booleans must be strictly equal
+
+    if (!compareComponents(x[String(p)], y[String(p)])) {return false;}
+    // Objects and Arrays must be tested recursively
   }
+
+  for (const p in y) {
+    if (Object.prototype.hasOwnProperty.call(y, p) && !Object.prototype.hasOwnProperty.call(x, p)) {return false;}
+  }
+  // allows x[ p ] to be set to undefined
+
   return true;
 };
+
 export { compareComponents };
