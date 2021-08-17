23e225723a594da84f0126bb6b58f598fbcebf3e
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2021-08-17 11:40:11 +0430

    test: add tests (#24)

---
+++ test/ComponentProvider.spec.ts
@@ -0,0 +1,42 @@
+import { asyncapiYAMLWithoutComponents, asyncapiYAML } from './fixtures';
+import { parse } from '@asyncapi/parser';
+import { ComponentProvider } from '../src/ComponentProvider';
+import { isInComponents } from '../src/Utils';
+
+describe('ComponentProvider', () => {
+  it('should not contain any component from components section', async () => {
+    const asyncapiDocument = await parse(asyncapiYAMLWithoutComponents);
+    const componentProviderWithoutComponents = new ComponentProvider(asyncapiDocument);
+    for (const key of componentProviderWithoutComponents.parameters.keys()) {
+      expect(isInComponents(key)).toBe(false);
+    }
+    for (const key of componentProviderWithoutComponents.messages.keys()) {
+      expect(isInComponents(key)).toBe(false);
+    }
+    for (const key of componentProviderWithoutComponents.schemas.keys()) {
+      expect(isInComponents(key)).toBe(false);
+    }
+  });
+
+  it('should contain some components from components section', async () => {
+    const asyncapiDocument = await parse(asyncapiYAML);
+    const componentProviderWithComponents = new ComponentProvider(asyncapiDocument);
+    let inComponentsCounter = 0;
+    for (const key of componentProviderWithComponents.parameters.keys()) {
+      if (isInComponents(key)) {
+        inComponentsCounter++;
+      }
+    }
+    for (const key of componentProviderWithComponents.messages.keys()) {
+      if (isInComponents(key)) {
+        inComponentsCounter++;
+      }
+    }
+    for (const key of componentProviderWithComponents.schemas.keys()) {
+      if (isInComponents(key)) {
+        inComponentsCounter++;
+      }
+    }
+    expect(inComponentsCounter).toBeGreaterThan(0);
+  });
+});
