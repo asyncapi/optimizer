76821054967dca4b14fa950bdb469e1301da56b0
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2022-11-22 13:46:47 +0000

    fix: export types (#129)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { Optimizer } from './Optimizer';
-
-export { Optimizer };
+export { Optimizer } from './Optimizer';
+export { Output } from './Models';
+export type { Report, ReportElement, Options } from './Models';
