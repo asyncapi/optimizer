3ae3324058527dd681df6fade5d375545ca45567
Khuda Dad Nomani <khudadad414@gmail.com>
2023-09-05 12:29:18 +0100

    modify example

--- examples/index.js
+++ examples/index.js
@@ -1,59 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Optimizer } = require('../lib/Optimizer')
 
-const yaml = `
-asyncapi: 3.0.0
-info:
-  title: Account Service
-  version: 1.0.0
-  description: This service is in charge of processing user signups
-channels:
-  user/signedup:
-    address: user/signedup
-    messages:
-      subscribe.message:
-        $ref: '#/components/messages/UserSignedUp'
-operations:
-  user/signedup.subscribe:
-    action: send
-    channel:
-      $ref: '#/channels/user~1signedup'
-    messages:
-      - $ref: '#/components/messages/UserSignedUp'
-components:
-  messages:
-    UserSignedUp:
-      payload:
-        type: object
-        properties:
-          displayName:
-            type: string
-            description: Name of the user
-          email:
-            type: string
-            format: email
-            description: Email of the user
-    UserSignedUpV2:
-      payload:
-        type: object
-        properties:
-          displayName:
-            type: string
-            description: Name of the user
-          email:
-            type: string
-            format: email
-            description: Email of the user
-`
-const optimizer = new Optimizer(yaml)
+// read input.yaml file synconously and store it as an string
+const input = require('fs').readFileSync('./examples/input.yaml', 'utf8')
+const optimizer = new Optimizer(input)
 optimizer.getReport().then((report) => {
   console.log(JSON.stringify(report))
   const optimizedDocument = optimizer.getOptimizedDocument({
+    output: 'JSON',
     rules: {
       reuseComponents: true,
       removeComponents: true,
       moveToComponents: true,
     },
   })
-  console.log(optimizedDocument)
+  //store optimizedDocument as to output.yaml
+  require('fs').writeFileSync('./examples/output.yaml', optimizedDocument)
 })
